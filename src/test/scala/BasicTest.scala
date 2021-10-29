62d756023ab380b119c304a0b96588da48e8e454
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2021-10-29 18:33:20 -0400

    Forgot a import

--- src/test/scala/BasicTest.scala
+++ src/test/scala/BasicTest.scala
@@ -7,6 +7,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import scala.concurrent.duration._
 import scala.util.Random
+import scalaj.http.{Http, HttpOptions}
 
 class BasicTest extends Simulation {
 
