be213da9f68181b1aa4fe3cad33fc9a607d857da
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2021-11-13 02:29:47 -0500

    mjhkbjghnjmk,jnbhg

--- src/test/scala/BasicTest.scala
+++ src/test/scala/BasicTest.scala
@@ -36,8 +36,7 @@ class BasicTest extends Simulation {
   /*** Like example on how to manage sessions and minimize the load on a your 
   authentication service. Use this fonction if you dont want to load test auth. ***/
   def getSession() = {
-    val data = ujson.read(Http(authURL).asString.body)
-    data.str
+    (Http(authURL).asString.body).jsonPath("$.session.token")
   }
 
   /*** HTTP Calls ***/
