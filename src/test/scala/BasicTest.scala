ca681fa7541ead515934a5bf91bda0fe0d3d9d69
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2021-11-15 16:37:05 -0500

    testing getSession

--- src/test/scala/BasicTest.scala
+++ src/test/scala/BasicTest.scala
@@ -36,6 +36,7 @@ class BasicTest extends Simulation {
     println(s"Running test with ${userCount} users")
     println(s"Ramping users over ${rampDuration} seconds")
     println(s"Total Test duration: ${testDuration} seconds")
+    println(getSession())
   }
 
   /*** Like example on how to manage sessions and minimize the load on a your 
